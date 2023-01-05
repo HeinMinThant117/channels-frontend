7316331b2b1bbf96406d3c6842de941256e9bb5c
heinminthant <heinminthant.17199@gmail.com>
2023-01-05 13:28:09 +0630

    add - layout of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
-import "./App.css";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App(): JSX.Element {
-  return <Heading>Hi. This is made with chakra ui</Heading>;
+  return (
+    <Grid h="100vh" templateRows="60px 1fr" templateColumns="20% 1fr">
+      <GridItem colSpan={3}>Nav Bar</GridItem>
+      <GridItem>Contacts</GridItem>
+      <GridItem>Chat Area</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
