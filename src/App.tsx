3d1869de68366aae7b0935bf4c2bab6cb7032d21
heinminthant <heinminthant.17199@gmail.com>
2023-01-05 15:41:45 +0630

    rough styling of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import {
+  Avatar,
   Box,
+  Button,
   Flex,
   Grid,
   GridItem,
   Heading,
   Image,
+  List,
+  ListItem,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
 
 function App(): JSX.Element {
@@ -41,17 +46,60 @@ function App(): JSX.Element {
             <Text fontSize="sm" mr={6} fontWeight="semibold">
               Mob Psycho
             </Text>
-            <Image
-              boxSize={6}
+            <Avatar
+              boxSize={7}
               mb={1}
-              borderRadius="2xl"
               src="https://avatars.dicebear.com/api/pixel-art/avdkfjdjdjdjd.svg"
             />
           </Flex>
         </Flex>
       </GridItem>
-      <GridItem>Contacts</GridItem>
-      <GridItem>Chat Area</GridItem>
+      <GridItem borderRight="1px solid #eee">
+        <Flex h="98%" direction="column" justifyContent="space-between">
+          <List p={3}>
+            <ListItem cursor="pointer">
+              <Text fontWeight="bold" fontSize="lg">
+                Channel 1
+              </Text>
+            </ListItem>
+          </List>
+          <Button colorScheme="purple" mx={2}>
+            Add Channel
+          </Button>
+        </Flex>
+      </GridItem>
+      <GridItem>
+        <Flex direction="column" justifyContent="space-between" h="100%" mx={4}>
+          <Box mt={4}>
+            <Flex alignItems="center">
+              <Avatar
+                src="https://avatars.dicebear.com/api/pixel-art/avdkfjdjdjdjd.svg"
+                boxSize={10}
+              />
+              <Box ml={2}>
+                <Text fontWeight="semibold" fontSize="sm">
+                  Mob Psycho
+                </Text>
+                <Text color="GrayText" fontSize="xs">
+                  10:50 AM
+                </Text>
+              </Box>
+            </Flex>
+            <Text mt={2} fontSize="sm">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
+              officia asperiores expedita. Nam mollitia nobis, perferendis
+              officiis temporibus, quod delectus ex ea dolorem reprehenderit
+              corporis? Asperiores voluptates alias culpa hic.
+            </Text>
+          </Box>
+          <Flex direction="column" h="140px">
+            <Textarea resize="none" />
+            <Button mt={2} ml="auto">
+              Send
+            </Button>
+          </Flex>
+        </Flex>
+      </GridItem>
     </Grid>
   );
 }
